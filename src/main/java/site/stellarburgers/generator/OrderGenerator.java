a12e87279818eb36e98e4b48fc39c844969c9e47
Anton <anton.zharkov1994@mail.ru>
2024-09-28 16:26:49 +0300

    its

--- src/main/java/site/stellarburgers/generator/OrderGenerator.java
+++ src/main/java/site/stellarburgers/generator/OrderGenerator.java
@@ -1,15 +1,12 @@
 package site.stellarburgers.generator;
 
+
 import site.stellarburgers.pojo.CreateOrder;
-import org.apache.http.client.methods.CloseableHttpResponse;
-import org.apache.http.client.methods.HttpGet;
-import org.apache.http.impl.client.CloseableHttpClient;
-import org.apache.http.impl.client.HttpClients;
-import org.apache.http.util.EntityUtils;
+import io.restassured.RestAssured;
+import io.restassured.response.Response;
 import org.json.JSONArray;
 import org.json.JSONObject;
 
-import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -18,7 +15,7 @@ public class OrderGenerator {
     final static String INCORRECT_INGREDIENT_HASH = "abc";
     final static String INGREDIENTS_URL = "https://example.com/api/ingredients"; // Замените на ваш URL
 
-    public static CreateOrder getDefaultOrder() throws IOException {
+    public static CreateOrder getDefaultOrder() {
         List<String> validIngredients = getValidIngredients();
         if (!validIngredients.isEmpty()) {
             return new CreateOrder(List.of(validIngredients.get(0)));
@@ -35,18 +32,14 @@ public class OrderGenerator {
         return new CreateOrder(List.of(INCORRECT_INGREDIENT_HASH));
     }
 
-    private static List<String> getValidIngredients() throws IOException {
+    private static List<String> getValidIngredients() {
         List<String> validIngredients = new ArrayList<>();
-        try (CloseableHttpClient httpClient = HttpClients.createDefault()) {
-            HttpGet request = new HttpGet(INGREDIENTS_URL);
-            try (CloseableHttpResponse response = httpClient.execute(request)) {
-                String responseBody = EntityUtils.toString(response.getEntity());
-                JSONArray ingredientsArray = new JSONArray(responseBody);
-                for (int i = 0; i < ingredientsArray.length(); i++) {
-                    JSONObject ingredient = ingredientsArray.getJSONObject(i);
-                    validIngredients.add(ingredient.getString("_id"));
-                }
-            }
+        Response response = RestAssured.get(INGREDIENTS_URL);
+        String responseBody = response.getBody().asString();
+        JSONArray ingredientsArray = new JSONArray(responseBody);
+        for (int i = 0; i < ingredientsArray.length(); i++) {
+            JSONObject ingredient = ingredientsArray.getJSONObject(i);
+            validIngredients.add(ingredient.getString("_id"));
         }
         return validIngredients;
     }

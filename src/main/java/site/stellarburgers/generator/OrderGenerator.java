0198a8c74d28ef9cdc5fddc3483501d257051d8f
Anton <anton.zharkov1994@mail.ru>
2024-10-05 14:16:51 +0300

    its

--- src/main/java/site/stellarburgers/generator/OrderGenerator.java
+++ src/main/java/site/stellarburgers/generator/OrderGenerator.java
@@ -1,27 +1,17 @@
 package site.stellarburgers.generator;
 
-
 import site.stellarburgers.pojo.CreateOrder;
-import io.restassured.RestAssured;
-import io.restassured.response.Response;
-import org.json.JSONArray;
-import org.json.JSONObject;
 
-import java.util.ArrayList;
 import java.util.List;
 
 public class OrderGenerator {
 
+    final static String DEFAULT_INGREDIENT_HASH = "61c0c5a71d1f82001bdaaa70";
     final static String INCORRECT_INGREDIENT_HASH = "abc";
-    final static String INGREDIENTS_URL = "https://example.com/api/ingredients"; // Замените на ваш URL
+
 
     public static CreateOrder getDefaultOrder() {
-        List<String> validIngredients = getValidIngredients();
-        if (!validIngredients.isEmpty()) {
-            return new CreateOrder(List.of(validIngredients.get(0)));
-        } else {
-            throw new IllegalStateException("No valid ingredients found");
-        }
+        return new CreateOrder(List.of(DEFAULT_INGREDIENT_HASH));
     }
 
     public static CreateOrder getOrderWithoutIngredients() {
@@ -32,15 +22,4 @@ public class OrderGenerator {
         return new CreateOrder(List.of(INCORRECT_INGREDIENT_HASH));
     }
 
-    private static List<String> getValidIngredients() {
-        List<String> validIngredients = new ArrayList<>();
-        Response response = RestAssured.get(INGREDIENTS_URL);
-        String responseBody = response.getBody().asString();
-        JSONArray ingredientsArray = new JSONArray(responseBody);
-        for (int i = 0; i < ingredientsArray.length(); i++) {
-            JSONObject ingredient = ingredientsArray.getJSONObject(i);
-            validIngredients.add(ingredient.getString("_id"));
-        }
-        return validIngredients;
-    }
 }
